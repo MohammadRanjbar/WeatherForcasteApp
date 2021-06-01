f4d33fbb8b93b7161a781c78a4fdfa235db75b28
MohammadRanjbar <85164166+MohammadRanjbar@users.noreply.github.com>
2021-06-02 01:02:53 +0430

    Update WeatherApis.kt

--- app/src/main/java/com/myapps/testapp/api/WeatherApis.kt
+++ app/src/main/java/com/myapps/testapp/api/WeatherApis.kt
@@ -8,7 +8,7 @@ interface WeatherApis {
 
     companion object {
         const val BASE_URL = "https:api.openweathermap.org/data/2.5/"
-        private const val API_KEY ="bc479749033681ec553da14282a1e425"
+        private const val API_KEY ="api_key_from_openweather"
     }
 
     @GET("weather?appid=$API_KEY")
